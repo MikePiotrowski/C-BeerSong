52c5ba5669977f3fb6430ad230a32c11dd6158d0
Michael Lee Piotrowski <mlp95747@gmail.com>
2022-12-28 14:06:34 -0800

    Add files via upload

---
+++ BeerSong/BeerSong/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class BeerSong
+{
+    public static void Main(string[] args)
+    {
+        Action<int> printSong = numOfBeers => {
+            string bottlePluralization = (numOfBeers == 1) ? "bottle" : "bottles";
+            string song = numOfBeers + " " + bottlePluralization + " of beer on the wall, "
+                         + numOfBeers + " " + bottlePluralization + " of beer.\n"
+                         + "Take one down, pass it around, ";
+
+            if (numOfBeers > 1)
+            {
+                bottlePluralization = (numOfBeers - 1 == 1) ? "bottle" : "bottles";
+                song += (numOfBeers - 1) + " " + bottlePluralization + " of beer on the wall.";
+            }
+            else
+            {
+                song += "no more bottles of beer on the wall.";
+            }
+            Console.WriteLine(song);
+        };
+
+        for (int numOfBeers = 99; numOfBeers > 0; numOfBeers--)
+        {
+            printSong(numOfBeers);
+        }
+    }
+}

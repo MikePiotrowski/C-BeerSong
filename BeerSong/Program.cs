36933d840f1a213e1d4d4dbcac349cd4f9d40a0c
Michael Lee Piotrowski <mlp95747@gmail.com>
2022-12-28 22:10:59 -0800

    Improved v.5
    Improved the code by having the output between each number of bottles to four seconds.
    Added The current Date and time. The date and time will be displayed in red.

--- BeerSong/Program.cs
+++ BeerSong/Program.cs
@@ -8,7 +8,14 @@ public class BeerSong
 {
     public static void Main(string[] args)
     {
+        DateTime today = DateTime.Now;
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(today);
+        Console.ResetColor(); // The current date and time will be output in red
+        Console.WriteLine();
+
         Action<int> printSong = numOfBeers => {
+
             string bottlePluralization = (numOfBeers == 1) ? "bottle" : "bottles";
             string song = numOfBeers + " " + bottlePluralization + " of beer on the wall, "
                          + numOfBeers + " " + bottlePluralization + " of beer.\n"
@@ -29,7 +36,8 @@ public class BeerSong
         for (int numOfBeers = 99; numOfBeers > 0; numOfBeers--)
         {
             printSong(numOfBeers);
-            Thread.sleep(4000); // Pause for seconds after every numbered bottle of beer.
+            Thread.Sleep(4000); // Pause for 4 seconds between each bottle of beer.
         }
     }
 }
+

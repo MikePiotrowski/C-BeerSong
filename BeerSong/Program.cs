7772cb38deb1d5901284820e7978981bb980f158
Michael Lee Piotrowski <mlp95747@gmail.com>
2022-12-28 23:23:18 -0800

    Improved v1.5.5
    Added code to make thew text green so that it is easier on the eyes

--- BeerSong/Program.cs
+++ BeerSong/Program.cs
@@ -16,6 +16,7 @@ public class BeerSong
 
         Action<int> printSong = numOfBeers => {
 
+            Console.ForegroundColor = ConsoleColor.Green; // All text will be in green.
             string bottlePluralization = (numOfBeers == 1) ? "bottle" : "bottles";
             string song = numOfBeers + " " + bottlePluralization + " of beer on the wall, "
                          + numOfBeers + " " + bottlePluralization + " of beer.\n"
